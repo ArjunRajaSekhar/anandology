eac5ea795f056d6cd41bc00b6107a8ca26c1c3e1
ARJUN RAJA SEKHAR <36049728+ArjunRajaSekhar@users.noreply.github.com>
2018-12-20 21:47:18 +0530

    implementation of GREP function

---
+++ grep.py
@@ -0,0 +1,15 @@
+def grep(x,s):
+    i=0
+    f=open(x).readlines()
+    l=len(f)
+    while i<l:
+        p=s in f[i]
+        if(p==True):
+            f[i]=f[i].strip('\n')
+            print (f[i])
+            i=i+1
+        else:
+            print("nothing matches")
+            break
+import sys
+grep(sys.argv[1],sys.argv[2])
